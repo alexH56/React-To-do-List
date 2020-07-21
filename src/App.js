e564097d543fe8890458931576f3152ef768b49d
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-07-20 22:30:56 -0400

    Made "Todo" componenets iterative by mapping tasks

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Todo from './components/Todo';
 
 function App (props) {
+  const taskList = props.tasks.map(task => (
+    <Todo
+      key={task.id}
+      id={task.id}
+      name={task.name}
+      done={task.done}
+    />
+  ));
+
   return (
     <div className='todoapp stack-large'>
       <h1>TodoMatic</h1>
@@ -47,21 +56,7 @@ function App (props) {
         className='todo-list stack-large stack-exception'
         aria-labelledby='list-heading'
       >
-        <Todo
-          name='Eat'
-          done={false}
-          id='todo-0'
-        />
-        <Todo
-          name='Sleep'
-          done={false}
-          id='todo-1'
-        />
-        <Todo
-          name='Repeat'
-          done={false}
-          id='todo-2'
-        />
+        {taskList}
       </ul>
     </div>
   );
