0e2b4f2c5302228745e5110ad0e0d21817c40cd8
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-07-20 22:32:08 -0400

    Store list of tasks in array; pass to App by props

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const taskArray = [
+  { id: 'todo-0', name: 'Eat', done: true },
+  { id: 'todo-1', name: 'Sleep', done: false },
+  { id: 'todo-2', name: 'Repeat', done: false }
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      tasks={taskArray}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
